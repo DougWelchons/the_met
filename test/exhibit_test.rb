34d30f58192f627d84ac711407d70dbba0dc3038
DougWelchons <doug.welchons@gmail.com>
2021-01-07 09:15:17 -0700

    Add exhibit tests

---
+++ test/exhibit_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+
+class ExhibitTest < MiniTest::Test
+
+  def test_it_exists
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_instance of Exhibit, exhibit
+  end
+
+  def test_it_has_readale_attributes
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
+  end
+end
