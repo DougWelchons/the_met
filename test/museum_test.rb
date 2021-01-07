4919b6d82a34db4ac74de75e16291708243434a6
DougWelchons <doug.welchons@gmail.com>
2021-01-07 10:31:32 -0700

    Add loto contestants test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -88,7 +88,6 @@ class MuseumTest < MiniTest::Test
   end
 
   def test_it_can_list_patrons_by_exhibit_intrest
-    skip
     dmns = Museum.new("Denver Museum of Nature and Science")
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
@@ -113,5 +112,28 @@ class MuseumTest < MiniTest::Test
                 imax => []}
 
     assert_equal expected, dmns.patrons_by_exhibit_interest
+  end
+
+  def test_it_can_have_tickect_lottery_contestants
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    patron_1 = Patron.new("Bob", 0)
+    patron_2 = Patron.new("Sally", 20)
+    patron_3 = Patron.new("Johnny", 5)
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    assert_equal [parton_1, patron_2], dmns.ticket_lottery_contestants(dead_sea_scrolls)
   end
 end
