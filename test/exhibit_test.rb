2318f51139f86e87549b3ce6b5eccd0e98202b4e
DougWelchons <doug.welchons@gmail.com>
2021-01-07 09:18:56 -0700

    Add typo corrections

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -7,7 +7,7 @@ class ExhibitTest < MiniTest::Test
   def test_it_exists
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-    assert_instance of Exhibit, exhibit
+    assert_instance_of Exhibit, exhibit
   end
 
   def test_it_has_readale_attributes
